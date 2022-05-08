546740321158a0da67b6a99db92dac50dda5ee4b
zaghloulegy <74609784+zaghloulegy@users.noreply.github.com>
2022-05-08 17:43:19 +0100

    s

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+import "./index.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
