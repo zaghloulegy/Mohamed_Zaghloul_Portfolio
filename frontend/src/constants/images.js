5e9ec735dcca7f4c2441051621478e9d2539e6e0
zaghloulegy <74609784+zaghloulegy@users.noreply.github.com>
2022-05-20 16:08:24 +0100

    clean

--- frontend/src/constants/images.js
+++ frontend/src/constants/images.js
@@ -18,23 +18,13 @@ import redux from "../assets/redux.png";
 import sass from "../assets/sass.png";
 import typescript from "../assets/typescript.png";
 import vue from "../assets/vue.png";
-
 import about01 from "../assets/about01.png";
 import about02 from "../assets/about02.png";
 import about03 from "../assets/about03.png";
 import about04 from "../assets/about04.png";
-
 import profile from "../assets/profile.png";
 import circle from "../assets/circle.svg";
 import logo from "../assets/logo.png";
-
-import adidas from "../assets/adidas.png";
-import amazon from "../assets/amazon.png";
-import asus from "../assets/asus.png";
-import bolt from "../assets/bolt.png";
-import nb from "../assets/nb.png";
-import skype from "../assets/skype.png";
-import spotify from "../assets/spotify.png";
 
 export default {
   email,
@@ -63,11 +53,4 @@ export default {
   profile,
   circle,
   logo,
-  adidas,
-  amazon,
-  asus,
-  bolt,
-  nb,
-  skype,
-  spotify,
 };
