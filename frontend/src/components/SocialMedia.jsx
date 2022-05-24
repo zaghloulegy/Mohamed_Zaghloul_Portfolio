17bb27e465c87759e1c964f133e27b81569546fb
zaghloulegy <74609784+zaghloulegy@users.noreply.github.com>
2022-05-24 14:51:40 +0100

    fix skill button

--- frontend/src/components/SocialMedia.jsx
+++ frontend/src/components/SocialMedia.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import { BsLinkedin, BsGithub } from "react-icons/bs";
 
 const SocialMedia = () => (
-  <div className="app__social">
-    <div>
-      <BsGithub />
-    </div>
+<div className="app__social">
+    <a href="https://github.com/zaghloulegy" target="_blank" rel="noreferrer">
+    <BsGithub />
+    </a>
 
-    <div>
-      <BsLinkedin />
-    </div>
-  </div>
+    <a
+    href="https://www.linkedin.com/in/zaghloulegy/"
+    target="_blank"
+    rel="noreferrer"
+    >
+    <BsLinkedin />
+    </a>
+</div>
 );
 
 export default SocialMedia;
