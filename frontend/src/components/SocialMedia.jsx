f502932a72d0df764f6e350c0176ef4f65c87d67
zaghloulegy <74609784+zaghloulegy@users.noreply.github.com>
2022-05-24 14:03:33 +0100

    chhange social media icons

--- frontend/src/components/SocialMedia.jsx
+++ frontend/src/components/SocialMedia.jsx
@@ -1,19 +1,16 @@
-    import React from "react";
-    import { BsTwitter, BsInstagram } from "react-icons/bs";
-    import { FaFacebookF } from "react-icons/fa";
+import React from "react";
+import { BsLinkedin, BsGithub } from "react-icons/bs";
 
-    const SocialMedia = () => (
-    <div className="app__social">
+const SocialMedia = () => (
+  <div className="app__social">
     <div>
-        <BsTwitter />
+      <BsGithub />
     </div>
-    <div>
-        <FaFacebookF />
-    </div>
-    <div>
-        <BsInstagram />
-    </div>
-    </div>
-    );
 
-    export default SocialMedia;
+    <div>
+      <BsLinkedin />
+    </div>
+  </div>
+);
+
+export default SocialMedia;
