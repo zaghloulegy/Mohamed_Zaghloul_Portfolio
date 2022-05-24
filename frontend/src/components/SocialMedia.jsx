89024272f0d6d15ef1aae621d7382ea9f4b5c1b6
zaghloulegy <74609784+zaghloulegy@users.noreply.github.com>
2022-05-24 14:57:55 +0100

    fix social media button

--- frontend/src/components/SocialMedia.jsx
+++ frontend/src/components/SocialMedia.jsx
@@ -2,19 +2,22 @@ import React from "react";
 import { BsLinkedin, BsGithub } from "react-icons/bs";
 
 const SocialMedia = () => (
-<div className="app__social">
-    <a href="https://github.com/zaghloulegy" target="_blank" rel="noreferrer">
-    <BsGithub />
-    </a>
-
-    <a
-    href="https://www.linkedin.com/in/zaghloulegy/"
-    target="_blank"
-    rel="noreferrer"
-    >
-    <BsLinkedin />
-    </a>
-</div>
+  <div className="app__social">
+    <div>
+      <a href="https://github.com/zaghloulegy" target="_blank" rel="noreferrer">
+        <BsGithub />
+      </a>
+    </div>
+    <div>
+      <a
+        href="https://www.linkedin.com/in/zaghloulegy/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <BsLinkedin />
+      </a>
+    </div>
+  </div>
 );
 
 export default SocialMedia;
